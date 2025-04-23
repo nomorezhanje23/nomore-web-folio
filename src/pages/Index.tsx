01c6a6d75a944f4fb2f9bcd87eab8b9dec636525
nomorezhanje23 <nomorezhanje23@gmail.com>
2025-04-24 00:04:35 +0200

    Title changed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,7 +17,7 @@ const Index = () => {
           <div className="max-w-2xl mx-auto text-center">
             <h2 className="text-3xl md:text-4xl font-bold mb-6">About Me</h2>
             <p className="text-white/80 mb-6 leading-relaxed">
-            Hi, I'm Nomore — a Web Frontend Engineer based in Cape Town.
+            Hi, I'm Nomore — a Software Engineer based in Cape Town.
             With a strong foundation in front-end development and a passion for creating seamless user experiences,
             I specialize in crafting responsive, high-performance web applications using HTML, CSS, JavaScript, React, and Bootstrap.
 
